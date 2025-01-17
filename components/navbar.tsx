96898b85cfe353c31c5119acd130660b56a6810b
Emilio Esposito <espo412@gmail.com>
2025-01-17 08:40:54 -0500

    branding

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -24,22 +24,24 @@ export const Navbar = () => {
           <span className="sr-only">Toggle sidebar</span>
         </Button>
 
-        {/* View source code button */}
-        <Link href="https://github.com/vercel-labs/ai-sdk-preview-python-streaming">
-          <Button variant="outline">
-            <GitIcon /> View Source Code
-          </Button>
-        </Link>
       </div>
 
       <div className="flex gap-2">
-        
-        <Link href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fvercel-labs%2Fai-sdk-preview-python-streaming&env=OPENAI_API_KEY%2CVERCEL_FORCE_PYTHON_STREAMING&envDescription=API+keys+needed+for+application&envLink=https%3A%2F%2Fgithub.com%2Fvercel-labs%2Fai-sdk-preview-python-streaming%2Fblob%2Fmain%2F.env.example&teamSlug=vercel-labs">
+
+
+        {/* View source code button */}
+        <Link href="https://github.com/EmilioEsposito/portfolio">
+          <Button variant="outline">
+            <GitIcon />Source Code
+          </Button>
+        </Link>
+
+        {/* <Link href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fvercel-labs%2Fai-sdk-preview-python-streaming&env=OPENAI_API_KEY%2CVERCEL_FORCE_PYTHON_STREAMING&envDescription=API+keys+needed+for+application&envLink=https%3A%2F%2Fgithub.com%2Fvercel-labs%2Fai-sdk-preview-python-streaming%2Fblob%2Fmain%2F.env.example&teamSlug=vercel-labs">
           <Button>
             <VercelIcon />
             Deploy with Vercel
           </Button>
-        </Link>
+        </Link> */}
       </div>
     </div>
   );
