d5b12d0d8f507f79c3fbff3a4c4014969768479a
Emilio Esposito <espo412@gmail.com>
2025-02-11 17:50:53 -0500

    better logging

--- run_db_migration.sh
+++ run_db_migration.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+echo "DB_MIGRATION: STARTING..."
 
 # Install dependencies
-pip3 install -r requirements.txt
+echo "DB_MIGRATION: INSTALLING DEPENDENCIES..."
+pip3 install -r requirements.txt 1> /dev/null
 
 # Set PYTHONPATH
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 
 # Run migrations
 cd api_src/database
-python3 -m alembic upgrade head
+python3 -m alembic upgrade head 1> /dev/null
+
+echo "DB_MIGRATION: COMPLETED"
