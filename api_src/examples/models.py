727ae003ca431fbcd2a731eeb903477f4184be1c
Emilio Esposito <espo412@gmail.com>
2025-02-11 18:00:43 -0500

    disable pip version check

--- api_src/examples/models.py
+++ api_src/examples/models.py
@@ -19,3 +19,21 @@ class Example(Base):
         server_default=func.now(),
         onupdate=func.now(),
     ) 
+
+
+class Example2(Base):
+    # Use lowercase for table name (will be unquoted by default)
+    __tablename__ = "example_neon2"
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String)
+    content: Mapped[str] = mapped_column(String)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), 
+        server_default=func.now(),
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), 
+        server_default=func.now(),
+        onupdate=func.now(),
+    ) 
