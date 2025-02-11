f03c00433462bba3fdbeb920a6bb3987c22f9d05
Emilio Esposito <espo412@gmail.com>
2025-02-11 18:09:15 -0500

    sqlalchemy

--- run_db_migration.sh
+++ run_db_migration.sh
@@ -8,6 +8,10 @@ pip3 install -r requirements.txt --quiet --disable-pip-version-check --root-user
 # Set PYTHONPATH
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 
+# Suppress SQLAlchemy warnings
+export SQLALCHEMY_WARN_20=1
+export PYTHONWARNINGS="ignore:.*:sqlalchemy.exc.SAWarning"
+
 # Run migrations
 cd api_src/database
 python3 -m alembic --quiet upgrade head
