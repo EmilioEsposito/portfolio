eee0d677a7fb6fdbb75448e82d44a24a7c7f80a8
Emilio Esposito <espo412@gmail.com>
2025-02-19 09:29:38 -0500

    google oauth working

--- api_src/utils/clerk.py
+++ api_src/utils/clerk.py
@@ -4,6 +4,14 @@ from typing import Annotated, Optional
 from fastapi import Depends, HTTPException, Header, Request, status
 from clerk_backend_api import Clerk, Session, AuthenticateRequestOptions, RequestState
 import os
+from google.oauth2 import id_token
+from google.auth.transport import requests
+from google.oauth2.credentials import Credentials
+from typing import Union
+import logging
+
+logger = logging.getLogger(__name__)
+
 # Initialize Clerk client - in production, this would use os.environ
 
 clerk_secret_key = os.getenv("CLERK_SECRET_KEY")
@@ -12,6 +20,7 @@ if not clerk_secret_key:
 
 clerk_client = Clerk(bearer_auth=clerk_secret_key)
 
+# Documentation: https://github.com/clerk/clerk-sdk-python?tab=readme-ov-file#sdk-installation
 
 async def get_auth_state(
     request: Request
@@ -85,3 +94,39 @@ async def get_auth_user(
         user = clerk_client.users.get(user_id=user_id)
     
     return user
+
+
+
+async def get_google_credentials(
+    request: Request
+):
+    """
+    FastAPI dependency that returns the Google credentials for the authenticated user.
+    """
+    auth_state = await get_auth_state(request)
+
+    if not auth_state.is_signed_in:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User is not signed in.",
+        )
+    else:
+        user_id = auth_state.payload['sub']
+        list_creds = clerk_client.users.get_o_auth_access_token(user_id=user_id, provider="oauth_google")
+
+        if len(list_creds) == 0:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="User has no Google credentials.",
+            )
+        elif len(list_creds) > 1:
+            logger.warning(f"User {user_id} has multiple Google credentials, using the first one.")
+        
+        creds_resp = list_creds[0]
+        creds_dict = creds_resp.model_dump()
+        # creds_dict.keys()
+        # dict_keys(['object', 'external_account_id', 'provider_user_id', 'token', 'provider', 'public_metadata', 'label', 'scopes', 'expires_at'])
+        
+        credentials = Credentials(creds_dict['token'])
+        return credentials
+    
