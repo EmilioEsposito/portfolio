c478ef403bc1988af49369fe5d0ea2c9b2fd60c4
Emilio Esposito <espo412@gmail.com>
2025-03-13 19:53:38 -0400

    simplify

--- portfolio.session.sql
+++ portfolio.session.sql
@@ -62,42 +62,6 @@ FROM
 LIMIT
     100;
 
--- unreplied emails within past week that were received >4 hours ago
-SELECT
-    -- e.id,
-    --     e.message_id,
-    -- e.received_date,
-    -- convert utc to ET
-    -- format date as Mar 3, 2:25pm
-    TO_CHAR(
-        e.received_date at time zone 'America/New_York',
-        'Mon DD, HH12:MIpm'
-    ) AS received_date_str,
-    e.subject -- re.id,
-    -- re.message_id,
-    -- re.received_date,
-    -- re.subject,
-FROM
-    email_messages AS e
-    LEFT JOIN email_messages AS re ON e.thread_id = re.thread_id
-    AND re.subject ilike 'Re%'
-    AND re.received_date > e.received_date
-    AND e.id <> re.id
-    AND re.received_date > CURRENT_DATE - INTERVAL '1 week'
-    -- AND re.received_date < CURRENT_TIMESTAMP - INTERVAL '24 hour' -- uncomment to backtest
-WHERE
-    TRUE
-    AND 'Label_5289438082921996324' = ANY(e.label_ids) -- label: zillowlisting
-    AND 'INBOX' = ANY(e.label_ids) -- label: inbox
-    AND e.subject ilike '%requesting%'
-    AND e.subject NOT ilike 'Re%'
-    AND e.received_date > CURRENT_DATE - INTERVAL '1 week'
-    AND e.received_date < CURRENT_TIMESTAMP - INTERVAL '4 hour'
-    AND re.id IS NULL -- no reply found!
-ORDER BY
-    e.received_date DESC
-LIMIT
-    10;
 
 SELECT
     received_date,
