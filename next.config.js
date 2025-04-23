11cb7e9df7920e26d2ce02ceeb7fd43eb68cebc4
Emilio Esposito <espo412@gmail.com>
2025-04-23 16:50:37 -0400

    migrate hypercorn and railway stuff

--- next.config.js
+++ next.config.js
@@ -3,11 +3,16 @@ const nextConfig = {
   rewrites: async () => {
     // Check for the *presence* of the environment variable
     const dockerEnvVarExists = !!process.env.DOCKER_ENV;
+    const railwayEnvVarExists = !!process.env.RAILWAY_ENVIRONMENT_NAME;
 
     console.log(`>>> next.config.js rewrite check: NODE_ENV=${process.env.NODE_ENV}, DOCKER_ENV=${process.env.DOCKER_ENV}, dockerEnvVarExists=${dockerEnvVarExists}`);
 
     let backendDestination;
-    if (dockerEnvVarExists) {
+
+    if (railwayEnvVarExists) {
+      // Assume if the variable exists, we are in Railway and use https!
+      backendDestination = "https://backend:8000/api/:path*";
+    } else if (dockerEnvVarExists) {
       // Assume if the variable exists, we are in Docker Compose
       backendDestination = "http://backend:8000/api/:path*";
     } else if (process.env.NODE_ENV === "development") {
