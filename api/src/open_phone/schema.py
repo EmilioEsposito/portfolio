1632304382647667a8560b86ec61af345b49cc17
Emilio Esposito <espo412@gmail.com>
2025-05-16 12:13:28 -0400

    fix webhook validation

--- api/src/open_phone/schema.py
+++ api/src/open_phone/schema.py
@@ -2,13 +2,40 @@ from pydantic import BaseModel, Field
 from typing import List, Optional, Dict, Any, Union
 from datetime import datetime
 
+# {
+#   "object": {
+#     "id": "EV9cf5849eaf5649aca0cf5e530935ab25",
+#     "object": "event",
+#     "createdAt": "2025-05-15T21:00:11.878Z",
+#     "apiVersion": "v3",
+#     "type": "contact.updated",
+#     "data": {
+#       "object": {
+#         "id": "CT6823ffc81fc6d26d75e6e008",
+#         "object": "contact",
+#         "firstName": "Lead 659-02 Shalece",
+#         "lastName": "C",
+#         "fields": {
+#           "Phone": "+14128535356"
+#         },
+#         "notes": [],
+#         "sharedWith": [
+#           "OR98t1AGEk"
+#         ],
+#         "clientId": "3a2c75be-1299-4fdb-b4c8-bccf2c6aafd2",
+#         "createdAt": "2025-05-14T02:28:24.277Z",
+#         "updatedAt": "2025-05-15T21:00:11.834Z",
+#         "userId": "USXAiFJxgv"
+#       }
+#     }
+#   }
+# }
+
 class BaseOpenPhoneObject(BaseModel):
     id: str
     object: str
     createdAt: datetime
     userId: str
-    phoneNumberId: Optional[str] = None
-    conversationId: Optional[str] = None
 
 class MessageObject(BaseOpenPhoneObject):
     from_: str = Field(..., alias="from")
@@ -18,6 +45,8 @@ class MessageObject(BaseOpenPhoneObject):
     status: str
     createdBy: Optional[str] = None
     direction: str
+    phoneNumberId: Optional[str] = None
+    conversationId: Optional[str] = None
 
 class CallObject(BaseOpenPhoneObject):
     from_: str = Field(..., alias="from")
@@ -36,7 +65,7 @@ class ContactObject(BaseOpenPhoneObject):
     company: Optional[str] = ""
     role: Optional[str] = ""
     pictureUrl: Optional[str] = ""
-    fields: Optional[List[Dict[str, Any]]] = []
+    fields: Optional[Dict[str, Any]] = []
     notes: List[Any] = []
     sharedWith: List[str]
     clientId: Optional[str] = ""
